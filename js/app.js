e5a66beb916350ba0622d06940a3fb46cbb36fac
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-19 21:11:14 -0500

    add: global environment and global object code

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//Global Environment and the Global Object
+
+//Global: Not Inside a Function
+var a = 'Hello World!';
+
+function b() {
+
+}
+//There is ALWAYS a global object b/c of the JavaScript engine
