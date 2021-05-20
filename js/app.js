ed9fd9728e0aebb4c4bd10c1fb28dd48615c6fb8
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-19 21:45:17 -0500

    feat: js and undefined

--- js/app.js
+++ js/app.js
@@ -3,17 +3,45 @@
 //There is ALWAYS a global object b/c of the JavaScript engine
 
 //The Execution Context: Creation and "Hoisting"
-b(); //Called b!
-console.log(a); //undefined
+//CODE:
+// b(); //Called b!
+// console.log(a); //undefined
+
 //You get "undefined" because of hoisting
 
 //Setup Memory Space for Variables and Functions -> referred to as "Hoisting"
 
 
 //In this scenario, the variable is saved and initially set to undefined. That's why we see undefined when we run the code above.
-var a = 'Hello World!';
+
+//CODE:
+// var a = 'Hello World!';
 
 //the entire function is saved in memory space
-function b() {
-    console.log('Called b!');
+
+//CODE:
+// function b() {
+//     console.log('Called b!');
+// }
+
+
+
+//JavaScript and 'undefined;
+
+//CODE:
+var a;
+// var a = "Hello World";
+console.log(a); //undefined
+
+if (a === undefined) {
+    console.log('a is undefined!');
 }
+else {
+    console.log('a is defined!');
+}
+
+
+//NEVER set a variable to undefined b/c it helps with debugging to let you know that you've never set the value of the variable
+
+//CODE:
+// a = undefined;
