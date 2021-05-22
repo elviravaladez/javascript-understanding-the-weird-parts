3436cbed3bd82ed02ea32c73c1bee30e236feaec
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-22 03:02:50 -0500

    remove: extra t in set

--- js/lib2.js
+++ js/lib2.js
@@ -1,2 +1,2 @@
 window.libraryName = window.libraryName || "Lib 2";
-//checking to see if libraryName is already set in global scope. If not, sett it to "Lib 2"
+//checking to see if libraryName is already set in global scope. If not, set it to "Lib 2"
