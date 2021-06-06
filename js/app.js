b1bfffb39a9fb824c144782e6ee6da3e874c80bb
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-06-05 23:01:52 -0500

    feat: polyfill

--- js/app.js
+++ js/app.js
@@ -1,21 +1,48 @@
-//Object.create and Pure Prototypal Inheritance
+// //Object.create and Pure Prototypal Inheritance
+//
+// //Another way to create objects: Object.create
+// var person = {
+//     firstName: 'Default',
+//     lastName: 'Default',
+//     greet: function () {
+//         return 'Hi ' + this.firstName;
+//     }
+// }
+//
+// var john = Object.create(person);
+// console.log(john);//{}
+//
+// //Object.create creates an empty object with its prototype pointing at whatever you pass into Object.create
+// //you override whatever you want to by adding the properties and methods to the object
+// //Ex (Pure Prototypal Inheritance):
+// john.firstName = 'John';
+// john.lastName = 'Doe';
+// console.log(john);//{firstName: "John", lastName: "Doe"}
+// console.log(john.greet());//Hi John
 
-//Another way to create objects: Object.create
+
+// POLYFILL
+if (!Object.create) {
+    Object.create = function (o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation'
+                + ' only accepts the first parameter.');
+        }
+        function F() {}
+        F.prototype = o;
+        return new F();
+    };
+}
+
 var person = {
-    firstName: 'Default',
-    lastName: 'Default',
-    greet: function () {
-        return 'Hi ' + this.firstName;
+    firstname: 'Default',
+    lastname: 'Default',
+    greet: function() {
+        return 'Hi ' + this.firstname;
     }
 }
 
 var john = Object.create(person);
-console.log(john);//{}
-
-//Object.create creates an empty object with its prototype pointing at whatever you pass into Object.create
-//you override whatever you want to by adding the properties and methods to the object
-//Ex (Pure Prototypal Inheritance):
-john.firstName = 'John';
-john.lastName = 'Doe';
-console.log(john);//{firstName: "John", lastName: "Doe"}
-console.log(john.greet());//Hi John
+john.firstname = 'John';
+john.lastname = 'Doe';
+console.log(john);
