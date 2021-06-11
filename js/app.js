bcd7f7b24f55d651609a8915c2ff151ac51a405d
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-06-10 22:28:19 -0500

    feat: jQuery

--- js/app.js
+++ js/app.js
@@ -1,13 +1,2 @@
-//strict mode
-"use strict";
-function logNewPerson() {
-    var person2;
-    persom2 = {};
-    console.log(persom2);
-}
-
-var person;
-//miss-typed the name to show how use strict is working
-persom = {};
-console.log(persom);
-logNewPerson();
+var q = $("ul.people li");
+console.log(q);
